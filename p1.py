2d3c9089360ab1371b3ed672722b96cca12c50c2
Mona Lisa <anitaimanbeig@gmail.com>
2025-09-15 10:52:00 -0400

    created a python file to be creepy.

---
+++ p1.py
@@ -0,0 +1,8 @@
+# My first program
+# Created on 09/15/25
+# Purpose: to leaarn python 
+# Editor: Anita Imanbeigi
+
+name = input("Hey there, can I have your name? : ")
+age = input("Can I have your age? : ")
+print(f"Hi {name} it's so nice to meet you! You are {age} years old! ")
