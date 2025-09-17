2862bf97d46bec0912dec86040448074f04b192d
Mona Lisa <anitaimanbeig@gmail.com>
2025-09-17 10:51:30 -0400

    made edits to my python files

---
+++ p4.py
@@ -0,0 +1,18 @@
+# factors of a number
+
+import math
+start = 1 
+num = int(input("Enter a value: "))
+new_stop = int(math.sqrt(num)) + 1
+factor_count = 0
+while start < new_stop:
+    if num % start == 0:
+        dividend = num // start 
+        if start != dividend:
+            factor_count += 2
+        else: 
+            factor_count += 1
+
+    start += 1
+print (f"{num} has {factor_count} factors.")
+
